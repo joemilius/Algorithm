5a7cbf340e5277b9a1a2c1447f69c682da88d7f9
JoeMilius <joemilius@gmail.com>
2021-11-09 10:59:00 -0700

    remove falsy values from an array

---
+++ Javascript/RemoveFalsy.js
@@ -0,0 +1,17 @@
+function bouncer(arr) {
+    //  let truthy = [] 
+    //   for(let i = 0; i < arr.length; i++){
+    //       if(arr[i]){
+    //         truthy.push(arr[i])
+            
+    //     }
+       
+    //   }
+    //  console.log(truthy)
+      console.log (arr.filter(Boolean));
+    }
+        
+    
+    bouncer([7, "ate", "", false, 9]);
+    bouncer([false, null, 0, NaN, undefined, ""])
+    bouncer(["a", "b", "c"]) 
