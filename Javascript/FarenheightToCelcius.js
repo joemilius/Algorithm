551d260b4f05c9e253b3dfff5a19711a68a6235f
JoeMilius <joemilius@gmail.com>
2021-11-08 18:23:30 -0700

    celsius to farenheight calculator

---
+++ Javascript/FarenheightToCelcius.js
@@ -0,0 +1,10 @@
+function convertToF(celsius) {
+    let fahrenheit = celsius * (9/5) + 32;
+    console.log(fahrenheit);
+  }
+  
+  convertToF(30);
+  convertToF(-30)
+  convertToF(-10)
+  convertToF(0)
+  convertToF(20)
