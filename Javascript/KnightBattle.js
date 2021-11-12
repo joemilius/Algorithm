40ac5b4cdf1b50374013296209a2a03fefe282ca
JoeMilius <joemilius@gmail.com>
2021-11-11 23:02:10 -0700

    explanation of knight battle added

--- Javascript/KnightBattle.js
+++ Javascript/KnightBattle.js
@@ -8,7 +8,18 @@ function knightBattle(array, p, r){
             seatAddedArray.pop()
             seatAddedArray[0] = seatAddedArray[0] + 1
         }else if(seatAddedArray[counter]){
-            
+
         }
     }
 }
+
+//unfinished!
+
+/*
+should compare values of an array to the values next to it.  if they are the same
+then one value is deleted and the other is incremented by 1 
+for each value in the collection of values that are the same 
+[8, 8, 8, 6] => [10, 6]
+[8, 8, 6, 8] => [10, 6]
+[1, 1, 2, 4, 2, 3, 4] => [3, 4, 2, 3, 4]
+*/
