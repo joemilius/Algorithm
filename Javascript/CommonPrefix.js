7658810f60f01a9a0d4f703cde499f979ee7df93
JoeMilius <joemilius@gmail.com>
2021-11-29 12:22:47 -0700

    commonPrefix

---
+++ Javascript/CommonPrefix.js
@@ -0,0 +1,14 @@
+function commonPrefix(strs){
+    let i = 0 
+    let sorted = strs.sort()
+    
+    while(i < sorted[0].length && sorted[0].charAt(i) === sorted[sorted.length - 1].charAt(i)){
+        i++
+    }
+    
+    
+    return sorted[0].substring(0 , i)
+}
+
+commonPrefix(['flower', 'flow', 'flight'])
+commonPrefix(['car', 'racecar', 'dog'])
