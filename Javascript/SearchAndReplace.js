5fa9b48d9743c16c7a64289596dcb317a50ec1fd
JoeMilius <joemilius@gmail.com>
2021-12-04 23:39:56 -0700

    search and replace revised

--- Javascript/SearchAndReplace.js
+++ Javascript/SearchAndReplace.js
@@ -1,16 +1,25 @@
 function myReplace(str, before, after) {
     let words = str.split(' ')
+    let newWord = after
+    
+    if(before.split('')[0] === before.split('')[0].toUpperCase()){
+      newWord = after.split('')
+      newWord.splice(0, 1, after[0].toUpperCase())
+      newWord = newWord.join('')
+      console.log(newWord)
+    }
     
     for(let i = 0; i < words.length; i++){
       if(words[i] === before){
-        words.splice(i, 1, after)
+        words.splice(i, 1, newWord)
       }
     }
     console.log(words.join(' '))
-    return str;
-}
+    return words.join(' ');
+  }
   
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
-myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
-myReplace("His name is Tom", "Tom", "john")
-myReplace("This has a spellngi error", "spellngi", "spelling")
+  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+  myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
+  myReplace("His name is Tom", "Tom", "john")
+  myReplace("This has a spellngi error", "spellngi", "spelling")
+  myReplace("Let us go to the store", "store", "mall")
