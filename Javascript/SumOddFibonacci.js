c6e4232c6682e0e1f456f5bd7ce6875c25cc1829
JoeMilius <joemilius@gmail.com>
2021-12-06 16:58:13 -0700

    alternative solution to SumOddFibonacci

--- Javascript/SumOddFibonacci.js
+++ Javascript/SumOddFibonacci.js
@@ -15,3 +15,32 @@ function sumFibs(num){
 
 sumFibs(1000)
 // => 1785
+
+// function sumFibs(num) {
+  
+//     let first = 0
+//     let second = 1
+//     let next = 1
+//     let oddNumbers = [1]
+  
+//     while(next <= num){
+//       console.log(next)
+//       if(next % 2 !== 0){
+//         oddNumbers.push(next) 
+//       }
+//       first = second
+//       second = next
+//       next = first + second
+//     }
+//     console.log(oddNumbers.reduce((previous, current) => previous + current))
+//     return oddNumbers.reduce((previous, current) => previous + current);
+//   }
+  
+//   sumFibs(4);
+//   // => 5
+//   sumFibs(10)
+//   // => 10
+//   sumFibs(1000)
+//   //=> 1785
+//   sumFibs(4000000)
+//   //=> 4613732
