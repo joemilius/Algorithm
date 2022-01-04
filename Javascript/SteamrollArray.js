850ffca866a0b75fec7a3ac361829654816a29d9
JoeMilius <joemilius@gmail.com>
2022-01-04 11:03:56 -0700

    steamroll array

---
+++ Javascript/SteamrollArray.js
@@ -0,0 +1,21 @@
+
+
+/*
+With .flat() method 
+
+function steamrollArray(arr) {
+  
+    let end = 0
+    
+    while(end < arr.length){
+      arr = arr.flat()
+      end++
+      console.log(arr)
+    }
+    
+    return arr;
+  }
+
+  
+Flatten a nested array. You must account for varying levels of nesting.
+  */
