ab57ecda73961a41e9223344a34a6db0cd2db408
JoeMilius <joemilius@gmail.com>
2022-01-07 00:00:12 -0700

    find the difference

---
+++ Javascript/FindTheDifference.js
@@ -0,0 +1,6 @@
+var findTheDifference = function(s, t) {
+    let firstArray = s.split('')
+    let secondArray = t.split('')
+    console.log(firstArray, secondArray)
+    for(let i = 0; i < secondArray.length; i++){}
+};
