a5c66bf956d7627c2bc2d7a4c472508ef90ed764
JoeMilius <joemilius@gmail.com>
2022-01-08 20:06:12 -0700

    faster guess number

--- Javascript/GuessTheNumber.js
+++ Javascript/GuessTheNumber.js
@@ -23,6 +23,25 @@
     return pick
 };
 
+/* Faster Solution
+var guessNumber = function(n) {
+    let left = 0;
+    let right = n;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (guess(mid) == 0) {
+            return mid;
+        } else if (guess(mid) == -1) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    return 0;
+*/
+
 /*
 We are playing the Guess Game. The game is as follows:
 
