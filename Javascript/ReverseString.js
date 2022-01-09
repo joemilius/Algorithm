77b7159f8694b32bac112ca4e4e085d062c4e8a8
JoeMilius <joemilius@gmail.com>
2022-01-08 20:15:43 -0700

    reverse string

--- Javascript/ReverseString.js
+++ Javascript/ReverseString.js
@@ -12,3 +12,20 @@ function reverseString(str) {
   reverseString("hello")
   reverseString("Howdy")
   reverseString("Greetings from Earth")
+
+/* Easier Solution
+
+var reverseString = function(s) {
+    return s.reverse()
+};
+
+reverseString(["H","a","n","n","a","h"])
+reverseString(["h","e","l","l","o"])
+
+*/
+
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+*/
